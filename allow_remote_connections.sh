a282a8f2a956291161e9a2684a2041c140315507
Eamon Ford <eamonford@gmail.com>
2016-12-21 18:20:40 -0800

    fix(remote-connections): change ip address to 0.0.0.0

--- allow_remote_connections.sh
+++ allow_remote_connections.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/host    all             all             127.0.0.1\/32            md5/host    all             all             172.17.0.1\/16            md5/g' /etc/postgresql/9.4/main/pg_hba.conf
+sed -i 's/host    all             all             127.0.0.1\/32            md5/host    all             all             0.0.0.0\/0            md5/g' /etc/postgresql/9.4/main/pg_hba.conf
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.4/main/postgresql.conf
